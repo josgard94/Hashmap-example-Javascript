dfe2b5c978db1ac1550fc67e91e157e9eeacae6e
Edgard Díaz <ediaz@bluepeople.com>
2022-01-13 03:23:33 -0600

    Added method for calculated for Lucas series by Josgard

--- hashmap-function.js
+++ hashmap-function.js
@@ -1,55 +1,74 @@
-const data = 'Factorial';
-
+const data = 'LucasSequence';
 
 function getLucasSequence (lk){
-  //TO DO compute Lucas sequence
-  return lk;
+  
+  const params = {
+    type: 'Lucas',
+    lk: lk
+  };
+  if(lk <= 0)
+    throw "The value must be a number greater than zero";   
+  sequence = computeSequence(params);
+  
+  return sequence;
 }
 
 const getFibonacciSequence = (lk) =>{
-    let sequence = [0,1];
-    let last = 0;
-    let current = 1;
-    if(lk <= 0)
-      throw "The value must be a number greater than zero"
-    
-    for(i = 3; i <= lk; i++){
-      let copyOfCurrent = current;
-      current = current+last;
-      sequence.push(current);
-      last = copyOfCurrent;
-    }
-
-    return sequence;
+  const params = {
+   type:'Fibonacci',
+    lk: lk
+  };
+  if(lk <= 0)
+    throw "The value must be a number greater than zero";   
+  let sequence = computeSequence(params);
+  return sequence;
 }
+
+
+const computeSequence = ({type, lk}) => {
+  
+  const TYPE = {
+    'Lucas' : [2, 1],
+    'Fibonacci':[0, 1]
+  };
+
+  let sequence = TYPE[type];
+  let last = sequence[0];
+  let current = sequence[1];
+
+  for(i = 3; i <= lk; i++){
+    let copyOfCurrent = current;
+    current = current+last;
+    sequence.push(current);
+    last = copyOfCurrent;
+  }
+  
+  return sequence;
+}
+
 
 
 const computeFactorial = (lk) => {
   let fact = 1;
-
   for(let i = 1; i <= lk; i++){
     fact = fact * i;
   }
-
   return fact;
 }
 
-
 const getFactorial = (lk) =>{
- 
-  let factorial  = lk >= 0 ? computeFactorial(lk): "No se puede calcular el factorial";
-  
+  let factorial  = lk >= 0 ? computeFactorial(lk): "Unable to calculate factorial";
   return factorial;
 }
 
 const DIFERENT_METHODS = {
-  'LucasSequence': (lk) => getFibonacciSequence(lk),
-  'FibonacciSequence': (lk) => getLucasSequence(lk),
+  'FibonacciSequence': (lk) => getFibonacciSequence(lk),
+  'LucasSequence': (lk) => getLucasSequence(lk),
   'Factorial': (lk) => getFactorial(lk)
 }
     
-const DEFAULT = "error el caso no existe";
+const DEFAULT = "Unsupported case";
     
-const result = DIFERENT_METHODS[data] ? (DIFERENT_METHODS[data](15)) : DEFAULT
+const result = DIFERENT_METHODS[data] ? (DIFERENT_METHODS[data](10)) : DEFAULT
     
 console.log(result)
